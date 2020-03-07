42da145e7d7b9fb73387fbddacce5357f0c4f943
Jamal Nabi <Jamalnabi07@gmail.com>
2020-03-07 16:31:22 +0530

    Simple exercise tracker buils using MERN stack

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+//import { Mongoose } from "mongoose";
+
+const express = require('express');
+const cors = require('cors');//used to cnosume rest api from outside server
+const mongoose = require('mongoose');//Returns singleton object of mongoose
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, userCreateIndex: true,  useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("Mongodb connection established successfully");
+});
+
+const exercisesRouter = require('./routers/exercise');
+const usersRouter = require('./routers/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ src/components/create-user.component.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+//import { render } from '@testing-library/react';
+
+export default class CreateUser extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = { username : "" };
+    }
+
+    onChange(event){
+
+        this.setState({ username : event.target.value });
+
+    }
+
+    onSubmit(event){
+        debugger;
+        event.preventDefault();
+        const user = {
+            username : this.state.username
+        }
+        axios.post("http://localhost:5000/users/add/",user)
+        .then( res => console.log(res.data, "user added to MongoDB"))
+        .catch(err => console.log(err));
+        this.setState({ username : "" })
+        console.log("User name is ",user);
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={ this.onSubmit}>
+                    <div className="form-group">
+                    <label>User Name :</label>
+                    <input onChange={ this.onChange } value={ this.state.username }
+                    className="form-control" required type="text" />
+                    </div>
+                    <div className="form-group">
+                      <input type="submit" className="btn btn-primary" required  value="Create User"/>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+
+}
